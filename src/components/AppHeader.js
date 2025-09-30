12909a36d8d493b03b30b99b69dddb0247b6e583
trabelsiiram <trabelsiiram9@gmail.com>
2025-09-30 13:17:12 +0100

    add translation support for breadcrumbs, loading indicators, and header navigation items

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import {
   CContainer,
@@ -18,18 +18,16 @@ import { cilBell, cilEnvelopeOpen, cilList } from '@coreui/icons'
 
 import { AppBreadcrumb } from './index'
 import { AppHeaderDropdown, AppHeaderDropdownManager } from './header/index'
-import { switchThemeMode } from '../actions/appActions'
 import { toggleCreateTicketModalOpen } from '../actions/ticketActions'
 import Logo from '../assets/images/logoh.png'
 
 const AppHeader = () => {
   const { t, i18n } = useTranslation()
   const headerRef = useRef()
-  const { colorMode, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
+  const { setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
   const dispatch = useDispatch()
   const { theme } = useSelector((state) => state.data)
   const { user } = useSelector((state) => state.auth)
-  const location = useLocation()
 
   useEffect(() => {
     document.addEventListener('scroll', () => {
@@ -51,22 +49,22 @@ const AppHeader = () => {
         <CHeaderNav className="d-none d-md-flex ms-4">
           <CNavItem>
             <CNavLink to="/dashboard" as={NavLink}>
-              Dashboard
+              {t('header.nav.dashboard')}
             </CNavLink>
           </CNavItem>
           <CNavItem>
             <CNavLink to="/employees/list" as={NavLink}>
-              Employees
+              {t('header.nav.employees')}
             </CNavLink>
           </CNavItem>
           <CNavItem>
             <CNavLink to="/projet/list" as={NavLink}>
-              Projects
+              {t('header.nav.projects')}
             </CNavLink>
           </CNavItem>
           <CNavItem>
             <CNavLink to="/tickets/list" as={NavLink}>
-              Tickets
+              {t('header.nav.tickets')}
             </CNavLink>
           </CNavItem>
         </CHeaderNav>
@@ -96,22 +94,34 @@ const AppHeader = () => {
           <CNavItem>
             <CNavLink onClick={() => dispatch(toggleCreateTicketModalOpen())}>
               <CButton color="primary " size="sm">
-                Créer
+                {t('header.actions.create')}
               </CButton>
             </CNavLink>
           </CNavItem>
           <CNavItem>
-            <CNavLink href="#">
+            <CNavLink
+              href="#"
+              aria-label={t('header.actions.notifications')}
+              title={t('header.actions.notifications')}
+            >
               <CIcon icon={cilBell} size="lg" />
             </CNavLink>
           </CNavItem>
           <CNavItem>
-            <CNavLink href="#">
+            <CNavLink
+              href="#"
+              aria-label={t('header.actions.tasks')}
+              title={t('header.actions.tasks')}
+            >
               <CIcon icon={cilList} size="lg" />
             </CNavLink>
           </CNavItem>
           <CNavItem>
-            <CNavLink href="#">
+            <CNavLink
+              href="#"
+              aria-label={t('header.actions.messages')}
+              title={t('header.actions.messages')}
+            >
               <CIcon icon={cilEnvelopeOpen} size="lg" />
             </CNavLink>
           </CNavItem>
