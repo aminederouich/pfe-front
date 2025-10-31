c6f2b8be0ae05f99e62b6344a5d752f99e03b39b
Mohamed Amine DEROUICH <mohamedaminederouich@outlook.com>
2025-10-31 17:23:24 +0100

    feat: refactor Dashboard component layout to use CContainer and CRow for improved structure

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { CCol, CContainer, CRow } from '@coreui/react'
+
 import UserScoreWidgetStats from '../../components/charts/userScoreWidgetStats'
+import { AllTicketsTypeStats } from '../../components/charts/AllTicketsTypeStats'
 
 const Dashboard = () => {
   return (
-    <>
+    <CContainer>
       <UserScoreWidgetStats forMe />
-    </>
+      <CRow>
+        <CCol md={4}>
+          <AllTicketsTypeStats />
+        </CCol>
+      </CRow>
+    </CContainer>
   )
 }
 
